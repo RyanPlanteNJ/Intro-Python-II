df5fea491efd0bc997fd5494e37252c14bfe0605
RyanPlanteNJ <ryanm.plante@gmail.com>
2020-09-10 22:31:44 -0400

    fixed issues. will play with it some more to add more depth to it

--- src/adv.py
+++ src/adv.py
@@ -7,13 +7,13 @@ from game import Game
 outside_start = Room("Dark Cave", "You awake in an unknown place. Unaware of your surroundings, you begin to look around for any clues")
 
 
-outside = Room("Outside Cave Entrance", "North of you, the cave mount beckons",['sticks','leaves'])
+outside = Room("Outside Cave Entrance", "North of you, the cave mount beckons", ['sticks', 'leaves'])
 
-foyer = Room("Foyer", "Dim light filters in from the south. Dusty passages run north and east", ['lamp','book','mirror'])
+foyer = Room("Foyer", "Dim light filters in from the south. Dusty passages run north and east", ['lamp', 'book', 'mirror'])
 
 overlook = Room("Grand Overlook", "A steep cliff appears before you, falling into the darkness. Ahead to the north, a light flickers in the distance, but there is no way across the chasm.")
 
-narrow =  Room("Narrow Passage", "The narrow passage bends here from west to north. The smell of gold permeates the air.")
+narrow = Room("Narrow Passage", "The narrow passage bends here from west to north. The smell of gold permeates the air.")
 
 treasure = Room("Treasure Chamber", "You've found the long-lost treasure chamber! Sadly, it has already been completely emptied by earlier adventurers. The only exit is to the south.")
 
@@ -48,7 +48,7 @@ treasure.s_to = narrow
 # If the user enters "q", quit the game.
 
 player = Player(outside_start)
-game  = Game(Player)
+game = Game(Player)
 print(player.current_room)
 direction = game.control_direction()
 
--- src/game.py
+++ src/game.py
@@ -3,7 +3,7 @@ from room import Room
 
 class Game(Player):
     def __init__(self, player_items=[], room_items=[]):
-        super().__init__(player_items)
+        super().__init__(player_items, room_items)
 
     def handle_items(self, tools):
         choose = input(
@@ -12,7 +12,6 @@ class Game(Player):
             picked = input(f'Pick which items you want to carry: {tools} >>>')
             try:
                 self.player_add(tools[int(picked)-1])
-                self.room_drop(int(picked)-1)
             except IndexError:
                 print('This item does not exist.')
                 pass
@@ -23,7 +22,6 @@ class Game(Player):
                 else:
                     drop = input(f'Which item from your storage do you want to drop : {self.player_items} >>>')
                     self.player_drop(int(drop)-1)
-                    self.room_add(tools[int(drop)-1])
             except IndexError:
                 pass
         elif choose == 'look in bag':
@@ -34,6 +32,12 @@ class Game(Player):
                     print(f'Current storage: {self.player_items}\n')
             except IndexError:
                 pass
-
+        if len(self.player_items) > 0:
+            print(f'\nCurrent storage: {self.player_items}\n')
+        else:
+            print('You might want to grab some stuff')
     def control_direction(self):
         return input('Pick a direction [n,s,e,w]: ')
+
+        #paresstr = input().split(" ")
+        #     if parsestr[1]
